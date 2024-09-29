de9daf82e4c3525b85486a297d0fa7f9f7e6ea12
BioTE <biote.ven@gmail.com>
2024-09-28 23:08:36 -0400

    +Modelo Datos

---
+++ test/src/app/models/task.model.ts
@@ -0,0 +1,22 @@
+export interface TaskModel {
+}
+
+
+export interface Skill {
+    name: string; // Nombre de la habilidad
+}
+  
+export interface Person {
+    fullName: string; // Nombre completo
+    age: number; // Edad
+    skills: Skill[]; // Habilidades (arreglo anidado)
+}
+  
+export interface Task {
+    id: number; // Identificador único
+    name: string; // Nombre de la tarea
+    dueDate: Date; // Fecha límite
+    assignedPersons: Person[]; // Personas asociadas
+    completed: boolean; // Estado de la tarea
+}
+  

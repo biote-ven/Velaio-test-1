36d6e61a05277d822fae8dd344ac7c87b67cd93e
BioTE <biote.ven@gmail.com>
2024-09-29 16:25:10 -0400

    +Componente Lista de Tareas

--- test/src/app/app-routing.module.ts
+++ test/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { TaskComponent } from './components/task/task.component';
 import { HomeComponent } from './components/home/home.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, // Ruta raíz para la página inicial
   { path: 'task', component: TaskComponent }, 
+  { path: 'tasklist', component: TaskListComponent }, 
   { path: '**', redirectTo: '' } // Redirección por defecto
 ];
 
